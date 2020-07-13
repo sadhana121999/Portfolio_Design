fb56fd4e11af4a2c002be851aaf0ba2ffc1821e8
sadhana121999 <sadhanalokesh1999@gmail.com>
2020-07-13 16:46:17 +0530

    Updates

---
+++ precache-manifest.852e781a9d035a0c843d43792cf42760.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "530669f357cf7577daa4f9c73d1c2e57",
+    "url": "/Portfolio_Design/index.html"
+  },
+  {
+    "revision": "1dcf8ce1c68e99100467",
+    "url": "/Portfolio_Design/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "af7da59633b7a44a7f03",
+    "url": "/Portfolio_Design/static/css/main.3c9a944c.chunk.css"
+  },
+  {
+    "revision": "1dcf8ce1c68e99100467",
+    "url": "/Portfolio_Design/static/js/2.e1c96f59.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio_Design/static/js/2.e1c96f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af7da59633b7a44a7f03",
+    "url": "/Portfolio_Design/static/js/main.bf64895e.chunk.js"
+  },
+  {
+    "revision": "9af47cb63cf42de7716f",
+    "url": "/Portfolio_Design/static/js/runtime-main.02da46a6.js"
+  },
+  {
+    "revision": "8488b60cd9505adbdebb44faeaed7b29",
+    "url": "/Portfolio_Design/static/media/dash (1).8488b60c.png"
+  },
+  {
+    "revision": "3da49c686de364f2e513abcafa25f3ca",
+    "url": "/Portfolio_Design/static/media/evverest.3da49c68.png"
+  },
+  {
+    "revision": "746076c7761bf9b354db49442d262435",
+    "url": "/Portfolio_Design/static/media/ingen.746076c7.png"
+  },
+  {
+    "revision": "8f7903e78f07bf983e74b385a3001f84",
+    "url": "/Portfolio_Design/static/media/kalp.8f7903e7.png"
+  },
+  {
+    "revision": "7f1310c182b250010826a28233f3d390",
+    "url": "/Portfolio_Design/static/media/maaya.7f1310c1.png"
+  },
+  {
+    "revision": "28686939aa4cc113236bd60e53ba52b6",
+    "url": "/Portfolio_Design/static/media/sadz.28686939.png"
+  },
+  {
+    "revision": "5c9445353f137d0b5427edecaccb7fe7",
+    "url": "/Portfolio_Design/static/media/succi.5c944535.jpg"
+  },
+  {
+    "revision": "0473917052431a4fac8341064c295c91",
+    "url": "/Portfolio_Design/static/media/wer-01.04739170.png"
+  }
+]);
