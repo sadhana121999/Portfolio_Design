825a4c90dd62ce14e0ded7275e1202093d5c34ee
sadhana121999 <sadhanalokesh1999@gmail.com>
2020-09-07 19:27:59 +0530

    Updates

---
+++ precache-manifest.a9a6fc677c7421eb55c2aee1bfb67a34.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e203b1b74319d92aac9d32bdd19918f2",
+    "url": "/Portfolio_Design/index.html"
+  },
+  {
+    "revision": "e09493d53ca8c8675663",
+    "url": "/Portfolio_Design/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a776c0786c8e74ccbbf8",
+    "url": "/Portfolio_Design/static/css/main.3c9a944c.chunk.css"
+  },
+  {
+    "revision": "e09493d53ca8c8675663",
+    "url": "/Portfolio_Design/static/js/2.b5c7786a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio_Design/static/js/2.b5c7786a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a776c0786c8e74ccbbf8",
+    "url": "/Portfolio_Design/static/js/main.72ff6038.chunk.js"
+  },
+  {
+    "revision": "9af47cb63cf42de7716f",
+    "url": "/Portfolio_Design/static/js/runtime-main.02da46a6.js"
+  },
+  {
+    "revision": "8488b60cd9505adbdebb44faeaed7b29",
+    "url": "/Portfolio_Design/static/media/dash (1).8488b60c.png"
+  },
+  {
+    "revision": "3da49c686de364f2e513abcafa25f3ca",
+    "url": "/Portfolio_Design/static/media/evverest.3da49c68.png"
+  },
+  {
+    "revision": "16d46c612e839a302f5a549418391cff",
+    "url": "/Portfolio_Design/static/media/hap.16d46c61.png"
+  },
+  {
+    "revision": "746076c7761bf9b354db49442d262435",
+    "url": "/Portfolio_Design/static/media/ingen.746076c7.png"
+  },
+  {
+    "revision": "8f7903e78f07bf983e74b385a3001f84",
+    "url": "/Portfolio_Design/static/media/kalp.8f7903e7.png"
+  },
+  {
+    "revision": "7f1310c182b250010826a28233f3d390",
+    "url": "/Portfolio_Design/static/media/maaya.7f1310c1.png"
+  },
+  {
+    "revision": "bb25b5b946b8ea413faacc5aab90fb05",
+    "url": "/Portfolio_Design/static/media/roadtrip.bb25b5b9.png"
+  },
+  {
+    "revision": "28686939aa4cc113236bd60e53ba52b6",
+    "url": "/Portfolio_Design/static/media/sadz.28686939.png"
+  },
+  {
+    "revision": "0473917052431a4fac8341064c295c91",
+    "url": "/Portfolio_Design/static/media/wer-01.04739170.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio_Design/precache-manifest.12249dddf254952618e3af9823df2c3b.js"
+  "/Portfolio_Design/precache-manifest.a9a6fc677c7421eb55c2aee1bfb67a34.js"
 );
 
 self.addEventListener('message', (event) => {
